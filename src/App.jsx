b94d938376f2fd3168a548f0f06ce5da8e4effcc
TheClover47 <flixsmc@gmail.com>
2024-05-27 16:32:20 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <nav className='navbar'>
+        <a href="/cels" target="_blank">
+          Ceļš
+        </a>
+        <a href="/kolaza" target="_blank">
+          Kolāža
+        </a>
+      </nav>
+      <h1>Mans ceļš uz Latvijas Universitāti</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
